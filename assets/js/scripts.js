364a555baa79bea78a871dfa2968343229f0077f
Mark Delos Reyes <makizworks@gmail.com>
2017-10-22 15:57:19 -0400

    log events on key press

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,13 +2,14 @@ $("li").click(function() {
     $(this).toggleClass("completed")
 })
 
-//use .stopPropagation() to stop bubbling
 $("span").click(function(e) {
-    //deleted lishould be deleted not display: none
-    //add callback function to remove li with nice fadeOut effect
     $(this).parent().fadeOut(function() {
-        //"this" is now referring to the parent element "li" not "span"
         $(this).remove();
     });
     e.stopPropagation();
+})
+
+//get text input when keypress
+$("input[type='text']").keypress(function(e) {
+    console.log(e)
 })
