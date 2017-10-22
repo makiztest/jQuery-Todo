6f026427fac1d26e7827dd85e28a9aafae883663
Mark Delos Reyes <makizworks@gmail.com>
2017-10-22 15:30:26 -0400

    use stopPropagation to stop bubbling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,9 @@
 $("li").click(function() {
     $(this).toggleClass("completed")
 })
+
+//use .stopPropagation() to stop bubbling
+$("span").click(function(e) {
+    alert("click")
+    e.stopPropagation();
+})
