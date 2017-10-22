37a88027c405b952bc08cbeb548536a65c2b2e76
Mark Delos Reyes <makizworks@gmail.com>
2017-10-22 16:21:23 -0400

    set an empty string in the input val

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,11 +10,10 @@ $("span").click(function(e) {
 })
 
 $("input[type='text']").keypress(function(e) {
-    //if keypress is equal to enter
     if(e.which === 13) {
-        //grab a new todo text
         var newTodo = $(this).val();
-        //create a new li and add to ul
+        //set as a setter. put an empty string in the input
+        $(this).val("")
         $("ul").append("<li>" + newTodo + "</li>")
     }
 })
