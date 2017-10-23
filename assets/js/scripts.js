53a17d7030c1d108f43bc94fe763a70cc9bc79a7
Mark Delos Reyes <makizworks@gmail.com>
2017-10-22 22:08:19 -0400

    animate plus icon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,7 @@ $("input[type='text']").keypress(function(e) {
         $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + newTodo + "</li>")
     }
 })
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeToggle();
+})
