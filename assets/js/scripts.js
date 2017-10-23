f4f708149f4cd7f8a546e5bb137a3bbb1681e0b3
Mark Delos Reyes <makizworks@gmail.com>
2017-10-22 21:39:18 -0400

    add font awesome icons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,6 +13,6 @@ $("input[type='text']").keypress(function(e) {
     if(e.which === 13) {
         var newTodo = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + newTodo + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + newTodo + "</li>")
     }
 })
